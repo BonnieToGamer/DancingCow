61d0a0c346973e32b345e7ec8914a7f3b87bcf41
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2020-10-20 19:30:13 +0200

    Added the source code

---
+++ Cow_Source/HahaFunnyCow/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace HahaFunnyCow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Rick = new AxWMPLib.AxWindowsMediaPlayer();
+            this.RickButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WMP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Rick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.Rick);
+            this.panel1.Controls.Add(this.RickButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.WMP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // Rick
+            // 
+            this.Rick.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rick.Enabled = true;
+            this.Rick.Location = new System.Drawing.Point(0, 0);
+            this.Rick.Name = "Rick";
+            this.Rick.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Rick.OcxState")));
+            this.Rick.Size = new System.Drawing.Size(600, 600);
+            this.Rick.TabIndex = 3;
+            // 
+            // RickButton
+            // 
+            this.RickButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RickButton.BackColor = System.Drawing.Color.Transparent;
+            this.RickButton.ForeColor = System.Drawing.Color.Transparent;
+            this.RickButton.Location = new System.Drawing.Point(590, 0);
+            this.RickButton.Name = "RickButton";
+            this.RickButton.Size = new System.Drawing.Size(10, 10);
+            this.RickButton.TabIndex = 2;
+            this.RickButton.UseVisualStyleBackColor = false;
+            this.RickButton.Click += new System.EventHandler(this.RickButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(83, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // WMP
+            // 
+            this.WMP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.WMP.Enabled = true;
+            this.WMP.Location = new System.Drawing.Point(96, 165);
+            this.WMP.Name = "WMP";
+            this.WMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP.OcxState")));
+            this.WMP.Size = new System.Drawing.Size(295, 191);
+            this.WMP.TabIndex = 0;
+            this.WMP.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dancing Cow";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Rick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private AxWMPLib.AxWindowsMediaPlayer WMP;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RickButton;
+        private AxWMPLib.AxWindowsMediaPlayer Rick;
+    }
+}
+
---
+++ Cow_Source/HahaFunnyCow/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+/*
+    Hello There, What are you doing?
+    Looking at my source code huh?
+    Well... Sice you're already here,
+    Take a look at my sphagetti ass code.
+*/
+
+namespace HahaFunnyCow
+{
+    public partial class Form1 : Form
+    {
+
+        // The boundry of the screen
+        int boundryX { get; set; }
+        int boundryY { get; set; }
+
+        // The direction of the cow
+        int vx { get; set; }
+        int vy { get; set; }
+
+        // The speed of the cow
+        List<int> choice = new List<int> { -5, 5 };
+        
+        // Used to make a random direction when created
+        Random r = new Random();
+        int indexX { get; set; }
+        int indexY { get; set; }
+
+        // Initialize the Form
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            // Fixes a weird bug where cow bounces back and forth repeatedly
+            Application.DoEvents();
+
+            // Gets the screen dimensions
+            boundryX = this.Size.Width;
+            boundryY = this.Size.Height;
+            // Label causes a weird bug if it's not present (exact same bug asApplication.DoEvents() fixes)
+            label1.Hide();
+
+            // General settings for the players
+            WMP.settings.autoStart = true;
+            Rick.settings.autoStart = true;
+            Rick.Hide();
+
+            // Makes the random start direction
+            indexX = r.Next(choice.Count);
+            indexY = r.Next(choice.Count);
+            vx = choice[indexX];
+            vy = choice[indexY];
+        }
+
+        // Starts the cow player
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            // Makes a Temporary file that starts playing then right after starting gets deleted
+            string strTempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Cow.mp4");
+            File.WriteAllBytes(strTempFile, Properties.Resources.dancing_cow);
+
+            WMP.URL = strTempFile;
+            WMP.settings.setMode("loop", true);
+
+            Thread.Sleep(10);
+            File.Delete(strTempFile);
+        }
+
+        // Checks wheter the button has been pressed, when it has the the program goes into fullscreen and plays a rickroll
+        private void RickButton_Click(object sender, EventArgs e)
+        {
+            // Changes players
+            WMP.Ctlcontrols.stop();
+            Rick.Show();
+
+            // Does the same as the start of the cow player
+            string strTempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Rick.mp4");
+            File.WriteAllBytes(strTempFile, Properties.Resources.Never_Gonna_Give_You_Up);
+
+            Rick.URL = strTempFile;
+
+            Thread.Sleep(10);
+            File.Delete(strTempFile);
+
+            
+        }
+
+        // Checks collisions
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Moves the cow
+            WMP.Left = WMP.Left + vx;
+            WMP.Top = WMP.Top + vy;
+            
+            // Again with the weird bug
+            label1.Text = "bruh";
+
+            // The actuall collision checking
+            if ((WMP.Left + WMP.Width) > boundryX || WMP.Left < 0)
+            {
+                vx *= -1;
+            }
+
+
+            if ((WMP.Top + WMP.Height) > boundryY || WMP.Top < 0)
+            {
+                vy *= -1;
+            }
+
+            // Checks if the rickroll has stopped and resumes the cow
+            if (Rick.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                WMP.Ctlcontrols.play();
+                Rick.Visible = false;
+            }
+        }
+    }
+}
